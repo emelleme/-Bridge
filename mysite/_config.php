9c06131080a791e3e53642a7cf3cfd0ec311bfb3
emelleme <lloyd@emelle.me>
2012-04-23 17:13:31 -0300

    Update mysite/_config.php

--- mysite/_config.php
+++ mysite/_config.php
@@ -3,25 +3,51 @@
 global $project;
 $project = 'mysite';
 
+/* What kind of environment is this: development, test, or live (ie, production */
+Director::set_environment_type("dev");
+
+// Use _ss_environment.php file for configuration include
+//require_once("conf/ConfigureFromEnv.php");
+
+$dbname = "SS_bridge";
+$server = "localhost";
+$username = "root";
+$password = "dev-toby";
+
+//global $database;
+//$database = $dbname;
+
+/* Cloud Values override */
+$dbname = (getenv("OPENSHIFT_DB_USERNAME") != FALSE) ? $_ENV['OPENSHIFT_DB_USERNAME'] : "SS_dbname";
+$server = (getenv('OPENSHIFT_DB_HOST') !== FALSE) ? $_ENV['OPENSHIFT_DB_HOST'] : "localhost";
+$username = (getenv("OPENSHIFT_DB_USERNAME") != FALSE) ? $_ENV['OPENSHIFT_DB_USERNAME'] : "root";
+$password = (getenv("OPENSHIFT_DB_PASSWORD") != FALSE) ? $_ENV['OPENSHIFT_DB_PASSWORD'] : "";
+
 global $databaseConfig;
 $databaseConfig = array(
-	"type" => 'MySQLDatabase',
-	"server" => 'localhost',
-	"username" => 'root',
-	"password" => 'dev-toby',
-	"database" => 'SS_bridge',
-	"path" => '',
+        "type" => 'MySQLDatabase',
+        "server" => $server,
+        "username" => $username,
+        "password" => $password,
+        "database" => $dbname,
+        "path" => '',
 );
 
-Object::add_extension('Member', 'EventMemberRole');
 MySQLDatabase::set_connection_charset('utf8');
-
-// Set the current theme. More themes can be downloaded from
-// http://www.silverstripe.org/themes/
-SSViewer::set_theme('simple');
+Object::add_extension('Member', 'EventMemberRole');
+// This line set's the current theme. More themes can be
+// downloaded online
+SSViewer::set_theme('blackcandy');
 
 // Set the site locale
 i18n::set_locale('en_US');
 
-// Enable nested URLs for this site (e.g. page/sub-page/)
-if (class_exists('SiteTree')) SiteTree::enable_nested_urls();
+// enable nested URLs for this site (e.g. page/sub-page/)
+SiteTree::enable_nested_urls();
+
+/* Set CMS Name */
+LeftAndMain::setApplicationName("µBridge");
+LeftAndMain::set_loading_image('http://www.emelle.me/assets/images/Logo.loading.png','height:100px;width:584px');
+
+/* Configure a default username and password to access the CMS on all sites in */
+Security::setDefaultAdmin('admin','password1');
