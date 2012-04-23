aba7a09b7b03e872940603f2ec1d29d7ec48e9db
emelleme <lloyd@emelle.me>
2012-04-23 17:46:44 -0300

    Adding php global var for Openshift

--- framework/main.php
+++ framework/main.php
@@ -20,6 +20,9 @@ if (version_compare(phpversion(), '5.3.2', '<')) {
 	die();
 }
 
+ini_set('display_errors', 1);
+date_default_timezone_set('America/New_York');
+
 /**
  * Main file that handles every page request.
  *
