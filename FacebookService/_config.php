b3e3fe1770354d05fe95295da6d0c90d576390cf
Lloyd Emelle <home@emelle.me>
2012-04-23 16:11:47 -0400

    Updates

---
+++ FacebookService/_config.php
@@ -0,0 +1,16 @@
+<?php
+
+/* 
+ * Facebook API
+*/
+
+/**
+ * Set the API Key token and secret for the single user application
+ *
+ * NOTE:    Login to your Facebook app account and setup your application before
+ *          setting the credentials and application keys
+ */
+ 
+ //Application Name: https://developers.facebook.com/
+FacebookAuthService::setAPICredentials('350602034985793','d3769b2cc635636826e2a40f4a7e618b');
+//To-do: add Facebook redirect variable
